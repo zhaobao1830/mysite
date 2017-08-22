79abf48825c43c4052689a6212a3b57d71319d99
zhaobao1830 <x5219438>
2017-08-22 23:13:22 +0800

    1

---
+++ myapp/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup (
+    name = 'two',
+    version='1.0.0',
+    author = 'zhaobao1830',
+    author_email='zhaobao1830@163.com',
+    url = 'http://www.xxxxx.com',
+    description='这是一个测试打包的过程',
+    py_modules = ['two'],
+)
--- myapp/two.py
+++ myapp/two.py
@@ -1,7 +1,9 @@
-import sys
+from importlib import reload
 from myapp import one
 print(one.name)
 if __name__ == '__main__':
     print('two模块是执行模块')
 else:
     print('two模块是被其他模块导入运行')
+
+reload(one)
